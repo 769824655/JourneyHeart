b1fd712173bd63eee762d153dc0ddcf7b8c3c7d7
Axmany <Leroy@LeroydeMacBook-Pro.local>
2014-06-04 09:39:49 +0800

    FirstVssion

---
+++ 心旅/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  心旅
+//
+//  Created by Leroy on 14-6-4.
+//  Copyright (c) 2014年 Leroy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 心旅/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  心旅
+//
+//  Created by Leroy on 14-6-4.
+//  Copyright (c) 2014年 Leroy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
